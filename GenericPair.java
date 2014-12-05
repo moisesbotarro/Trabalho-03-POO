0171316aa968d0b462a9c4298d79aa6c57c5e980
Moisés Botarro <moisesbotarro@MacBook-Pro-de-Moises.local>
2014-12-05 15:12:10 -0200

    Classe Generica GenericPair adicionada

---
+++ GenericPair.java
@@ -0,0 +1,38 @@
+// Classe para guardar um par de valores no estilo (First/Second)
+
+public class GenericPair<First, Second>{
+        
+        private First first; // Primeira posição do par
+        private Second second; // Segunda posição do par
+        
+        // Construtor
+        public GenericPair ( First first, Second second ){
+            
+            this.first = first;
+            this.second = second;
+        }
+        
+        // Método para retornar o primeiro objeto do pair
+        public First getFirst(){
+            
+            return first;
+        }
+        
+        // Método para retornar o segundo objeto do pair
+        public Second getSecond(){
+            
+            return second;
+        }
+        
+        // Método para inserir outro objeto na primeira posição do pair
+        public void setFirst( First newFirst ){
+            
+            this.first = newFirst;
+        }
+    
+        // Método para inserir outro objeto na segunda posição do pair
+        public void setSecond( Second newSecond ){
+            
+            this.second = newSecond;
+        }
+    }
