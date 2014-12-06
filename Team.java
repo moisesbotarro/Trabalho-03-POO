178fba07c220e9328ef6440a9ed6b57136fb8810
moisesbotarro <moisesbotarro@gmail.com>
2014-12-06 15:59:43 -0200

    Método resolveBatttle() adiciona 1XP para personagens do time que venceu e estão vivos

--- Team.java
+++ Team.java
@@ -54,9 +54,16 @@ public class Team implements Printable {
         // Guarda em result a diferença da média de HP dos personagens do time que chama o método em relação ao time B
         double result = this.getPoints() - teamB.getPoints();
         
-        // Caso a média de HP dos personagens do time que chamou o método seja mair -> time que chamou o método venceu
+        // Caso a média de HP dos personagens do time que chamou o método seja maior -> time que chamou o método venceu
         if ( result > 0 ){
             this.win++;
+            
+            // Os personagens do time que venceu ganham 1 ponto de experiência cada, desde que o personagem esteja vivo
+            for ( int i = 0; i < character.size(); i++ ) {
+                
+                if ( !characters.get(i).isDead() )
+                    characters.get(i).addXP(1);
+            }
         }
         
         // Caso a média de HP dos personagens dos dois times seja igual -> Ocorreu empate
